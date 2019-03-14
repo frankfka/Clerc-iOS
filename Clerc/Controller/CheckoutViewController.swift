817c0b9edf87b9e6037faa8c48a0194155f5f5aa
Frank Jia <jiafrank98@gmail.com>
2019-03-14 08:44:49 -0700

    payment flow completed

--- Clerc/Controller/CheckoutViewController.swift
+++ Clerc/Controller/CheckoutViewController.swift
@@ -46,6 +46,7 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
     // UI Elements
     @IBOutlet weak var paymentOptionButton: UIButton!
     @IBOutlet weak var payNowButton: UIButton!
+    @IBOutlet weak var totalCostLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,6 +57,9 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
             // Return if something is wrong
             navigationController?.popViewController(animated: true)
         }
+        
+        // Update total cost label
+        totalCostLabel.text = TextFormatterService.getCurrencyString(for: cost!)
         
         // Else continue with setup - this should be in an init() function once we present this modally
         let config = STPPaymentConfiguration.shared()
@@ -76,6 +80,7 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
     }
     
     @IBAction func payNowTapped(_ sender: Any) {
+        // TODO confirmation dialog
         self.paymentInProgress = true
         self.paymentContext!.requestPayment()
     }
@@ -95,15 +100,15 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
     func paymentContextDidChange(_ paymentContext: STPPaymentContext) {
         // Show "Loading"
         print("Loading Payment Context")
-        paymentOptionButton.titleLabel?.text = "Loading"
+        paymentOptionButton.setTitle("Loading", for: .normal)
         // If paymentContext has a saved payment information
         if let paymentOption = paymentContext.selectedPaymentMethod {
             print("Payment context loaded with saved payment method")
-            paymentOptionButton.titleLabel?.text = paymentOption.label
+            paymentOptionButton.setTitle(paymentOption.label, for: .normal)
         } else {
             // No saved payment information, prompt user to select a payment
             print("Payment context loaded without saved payment method")
-            paymentOptionButton.titleLabel?.text = "Select Payment"
+            paymentOptionButton.setTitle("Select Payment", for: .normal)
         }
     }
     
@@ -128,6 +133,7 @@ class CheckoutViewController: UIViewController, STPPaymentContextDelegate {
                 // Show confirmation screen
                 print("User payment succeeded")
                 ViewService.showHUD(success: true, message: "Payment Successful")
+                dismiss(animated: true, completion: nil) // TODO present success screen here
                 return
             case .userCancellation:
                 return // Do nothing
