8efd2036fe0dd747e7b3ad6d49f2dea92c8360e4
Frank Jia <jiafrank98@gmail.com>
2019-03-14 21:12:32 -0700

    added empty data set for home view

--- Clerc/Controller/HomeViewController.swift
+++ Clerc/Controller/HomeViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 import BarcodeScanner
 import AVFoundation
 import FirebaseFirestore
+import EmptyDataSet_Swift
 
 class HomeViewController: UIViewController {
     
     // Used in segue initialization to tell next controller what vendor was scanned
     var scannedVendor: Vendor?
-
+    
+    // UI Elements
+    @IBOutlet weak var pastTransactionsTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pastTransactionsTable.emptyDataSetSource = self
+        pastTransactionsTable.emptyDataSetDelegate = self
+        
     }
     
     // When shopping button is pressed, launch the barcode scanner view controller
@@ -50,7 +57,9 @@ class HomeViewController: UIViewController {
     }
 }
 
+//
 // MARK: Extension for all barcode methods (to identify a retailer)
+//
 extension HomeViewController: BarcodeScannerCodeDelegate, BarcodeScannerDismissalDelegate {
     
     // Function called when code is captured
@@ -87,6 +96,39 @@ extension HomeViewController: BarcodeScannerCodeDelegate, BarcodeScannerDismissa
     func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
         // Dismiss the view controller
         controller.dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension HomeViewController: EmptyDataSetSource, EmptyDataSetDelegate {
+    
+    func title(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        return NSAttributedString(string: "No Past Transactions")
+    }
+    
+    func description(forEmptyDataSet scrollView: UIScrollView) -> NSAttributedString? {
+        return NSAttributedString(string: "Clerc makes shopping fast & easy. Your past transactions will appear here.")
+    }
+    
+    func image(forEmptyDataSet scrollView: UIScrollView) -> UIImage? {
+        return UIImage(named: "Receipt Illustration")
+    }
+    
+    func backgroundColor(forEmptyDataSet scrollView: UIScrollView) -> UIColor? {
+        return UIColor.white
+    }
+    
+    func emptyDataSetWillAppear(_ scrollView: UIScrollView) {
+        pastTransactionsTable.separatorStyle = .none
+    }
+    
+    func emptyDataSetWillDisappear(_ scrollView: UIScrollView) {
+        pastTransactionsTable.separatorStyle = .singleLine
+    }
+    
+    // Move the view up a bit to make it look better
+    func verticalOffset(forEmptyDataSet scrollView: UIScrollView) -> CGFloat {
+        return -50.0
     }
     
 }
