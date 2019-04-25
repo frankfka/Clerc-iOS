fd96099da5057a61be24091dad2b9bb40e66ab9f
Frank Jia <jiafrank98@gmail.com>
2019-04-24 21:53:00 -0700

    live key:

--- Clerc/Constants/StripeConstants.swift
+++ Clerc/Constants/StripeConstants.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class StripeConstants {
     
-    static let PUBLISHABLE_KEY = "pk_test_LvuvRJTzvgVmxMHSFFnz5VqR00DCZ68QK6"
+    static let PUBLISHABLE_KEY = "pk_live_caJiQ6z9SPsQh8pXx5qbLAzI00uAHe2qjo"
     static let BACKEND_URL = "https://paywithclerc.appspot.com"
     static let APPLE_MERCHANT_ID = "merchant.com.clerc"
     static let DEFAULT_CURRENCY = "cad"
